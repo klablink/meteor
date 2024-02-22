1756128581e558e99e40645629a2f89db66e6598
plb666 <pbeato@kubix.it>
2024-02-22 15:46:22 +0100

    Changing the version of dependencies on the less package

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kubixlab:meteor-tool',
   summary: 'The Meteor command-line tool with fixes for klab',
-  version: '3.0.0-beta.0_1'
+  version: '3.0.0-beta.3_1'
 });
 
 Package.includeTool();
