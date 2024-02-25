0c2012df98a709ddeadce3ac8810359e27cdb3c8
plb666 <pbeato@kubix.it>
2024-02-25 16:45:46 +0100

    Bump Meteor 3.0-beta.4

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kubixlab:meteor-tool',
   summary: 'The Meteor command-line tool',
-  version: '3.0.0-beta.4_1',
+  version: '3.0.0-beta.4_2',
 });
 
 Package.includeTool();
