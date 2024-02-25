ee080afbe6b6630302b01281eb996c0c134ffeb9
Pierluigi Beato <pierluigibeato@MacBook-Pro-di-Pierluigi.local>
2024-02-25 11:53:36 +0100

    Bump Meteor 3.0-beta.4

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,7 @@
 Package.describe({
+  name: 'kubixlab:meteor-tool',
   summary: 'The Meteor command-line tool',
-  version: '3.0.0-beta.4',
+  version: '3.0.0-beta.4_1',
 });
 
 Package.includeTool();
